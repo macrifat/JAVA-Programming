87c35889b4053e60ccbed7a4a3abd701aaf66e53
Monsur Ahmed Chowdhury <62332686+macrifat@users.noreply.github.com>
2020-12-27 01:39:15 +0100

    Algorithm

---
+++ Algorithm/SumNumber.java
@@ -0,0 +1,28 @@
+package Algorithm;
+import java.util.Scanner;
+
+public class SumNumber {
+
+	public static void main(String[] args){
+		
+		Scanner input =new Scanner (System.in);
+		
+		int m = input.nextInt();
+		int n = input.nextInt();
+		int sum = 0;
+		for (int i = m; i <= n; i++) {
+			
+			if (i%2== 0) {
+				
+				sum = sum + i;	
+			}
+			
+		}
+		System.out.println(sum);
+		
+		
+	}
+	
+	
+
+}
---
+++ Algorithm/SumOfDigit.java
@@ -0,0 +1,26 @@
+package Algorithm;
+// sum of digit    123 to 1+2+3 
+
+import java.util.Scanner;
+
+public class SumOfDigit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		int sum = 0;
+		int rem ;
+		int temp ;
+		System.out.println("Please enter digit : ");
+		int n= input.nextInt();
+		temp =n;
+		
+		    while(temp!=0) {
+		       rem =  temp % 10;
+		       sum= sum + rem;
+		       temp = temp/10;
+		    }
+		    System.out.println("Sum of digit is : "+sum);
+	}
+
+}
